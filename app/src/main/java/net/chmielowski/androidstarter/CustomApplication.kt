c04c83f0a1ce1cee92c0a00b6929b9ae15435142
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2017-09-26 20:25:44 +0200

    Stetho configured

--- app/src/main/java/net/chmielowski/androidstarter/CustomApplication.kt
+++ app/src/main/java/net/chmielowski/androidstarter/CustomApplication.kt
@@ -2,20 +2,28 @@ package net.chmielowski.androidstarter
 
 import android.app.Activity
 import android.app.Application
+import com.facebook.stetho.Stetho
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasDispatchingActivityInjector
 import javax.inject.Inject
 
 
-open class CustomApplication : Application(), HasDispatchingActivityInjector {
-    override fun activityInjector(): DispatchingAndroidInjector<Activity> {
-        return dispatchingAndroidInjector
-    }
+class CustomApplication : Application(), HasDispatchingActivityInjector {
+    override fun activityInjector() = injector
 
-    @Inject lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Activity>
+    @Inject lateinit var injector: DispatchingAndroidInjector<Activity>
 
     override fun onCreate() {
         super.onCreate()
+        initDagger()
+        initStetho()
+    }
+
+    private fun initStetho() {
+        Stetho.initializeWithDefaults(this)
+    }
+
+    private fun initDagger() {
         DaggerAppComponent
                 .builder()
                 .application(this)
